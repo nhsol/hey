1c65fd2c6498461e638c9806f715518922048339
Filipas Egor <apple@MacBook-Pro-5.local>
2020-07-22 20:09:01 +0300

    mouseOver

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 
-// let toggler = document.getElementById('toggle-btn-about')
-//
-// toggler.onclick = openAbout()
-//
-// function openAbout() {
-//
-//   document.getElementById('about').classList.toggle('active');
-// }
+
+document.getElementById("li").onmouseover = function() {mouseOver()};
+document.getElementById("li").onmouseout = function() {mouseOut()};
+
+function mouseOver() {
+
+  document.getElementById("germanium").style.opacity = '1';
+}
+
+function mouseOut() {
+  document.getElementById("germanium").style.opacity = '0';
+}
 
 
 function openAbout() {
 
-  document.getElementById("about").classList.toggle('active')
+  document.getElementById("about-us").classList.toggle('active');
+  document.getElementById("li").classList.toggle('active');
+  document.getElementById("germanium").classList.toggle('active');
+  return mouseOver();
 
 }
 
 
 function closeAbout() {
 
-  document.getElementById("about").classList.toggle('close')
+  document.getElementById("about-us").classList.remove('active');
+  document.getElementById("li").classList.remove('active');
+  document.getElementById("germanium").classList.remove('active')
 
 }
